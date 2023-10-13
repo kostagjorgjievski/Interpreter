0161cbaa94ca228f1206a856e30136d3f0a7b8ee
Kosta Gjorgjievski <kostagj@g.ucla.edu>
2023-10-13 14:44:04 -0700

    Variable addition is possible

--- interpreterv1.py
+++ interpreterv1.py
@@ -26,7 +26,7 @@ class Interpreter(InterpreterBase):
         # if expression type is function call, call another func to evalue
         expression_type = None
         if statement_expression.elem_type in ["+", "-"]:
-            self.variable_to_value[statement_name] = self.evaluate_expression_assignment(statement_expression)
+            self.variable_to_value[statement_name] = self.evaluate_expression_operation(statement_expression)
             print(self.variable_to_value)
         elif statement_expression.elem_type in "fcall":
             self.variable_to_value[statement_name] = self.evaluate_expression_function_call(statement_expression)
@@ -35,7 +35,8 @@ class Interpreter(InterpreterBase):
             self.error(ErrorType.TYPE_ERROR, "Invalid statement for " + statement_name + ". The staement must match '+', '-', or 'fcall'.")
 
 
-        print(self.variable_to_value)
+        for var, val in self.variable_to_value.items():
+            print(var + " : " + str(val))
         return
 
 
@@ -64,38 +65,39 @@ class Interpreter(InterpreterBase):
 
         first_value = None
         op1_type = self.determine_node_type(expression.get("op1"))
+        print(op1_type)
         if op1_type == "var":
             var_name = expression.get("op1").dict
             if var_name not in self.variable_to_value:
                 self.error(ErrorType.NAME_ERROR, "Variable does not exist in map")
             first_value = variable_to_value[var_name]
-        elif op1_type == "int":
-            first_value = expression.get("op1").dict
-        elif op1_type == "string":
-            first_value = expression.get("op1").dict
+        elif op1_type == "value_int":
+            first_value = expression.get("op1").dict.get("val")
+        elif op1_type == "value_string":
+            first_value = expression.get("op1").dict.get("val")
         elif op1_type == "expression_operation":
             first_value = evaluate_expression_operation(expression.get("op1"))
         elif op1_type == "expression_fcall":
             first_value = evaluate_expression_function_call(expression.get("op1"))
         else:
-            self.error(ErrorType.NAME_ERROR, "Operation value does not exist")
+            self.error(ErrorType.NAME_ERROR, "Operation value does not exist: " + op1_type )
 
 
         second_value = None
         op2_type = self.determine_node_type(expression.get("op2"))
         if op2_type == "var":
-            var_name = expression.get("op1").dict
+            var_name = expression.get("op2").dict
             if var_name not in self.variable_to_value:
                 self.error(ErrorType.NAME_ERROR, "Variable does not exist in map")
             second_value = variable_to_value[var_name]
-        elif op2_type == "int":
-            second_value = expression.get("op1").dict
-        elif op2_type == "string":
-            second_value = expression.get("op1").dict
+        elif op2_type == "value_int":
+            second_value = expression.get("op2").dict.get("val")
+        elif op2_type == "value_string":
+            second_value = expression.get("op2").dict.get("val")
         elif op2_type == "expression_operation":
-            second_value = evaluate_expression_operation(expression.get("op1"))
+            second_value = evaluate_expression_operation(expression.get("op2"))
         elif op2_type == "expression_fcall":
-            second_value = evaluate_expression_function_call(expression.get("op1"))
+            second_value = evaluate_expression_function_call(expression.get("op2"))
         else:
             self.error(ErrorType.NAME_ERROR, "Operation value does not exist")
 
@@ -103,9 +105,9 @@ class Interpreter(InterpreterBase):
 
         if type(first_value) != type(second_value):
             self.error(ErrorType.TYPE_ERROR, "Expression Type Error Line 79")
-        if operation == "+":
+        if mathematical_operation == "+":
             return first_value + second_value
-        if operation == "-":
+        if mathematical_operation == "-":
             return first_value - second_value
          
         return 0
